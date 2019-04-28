486dc88c6bb771f277155ed2a6bdade29d850d31
Adriana Bg <46886227+adrianabg@users.noreply.github.com>
2019-04-28 18:21:10 +0200

    Add files via upload

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,4 @@
+var car;
 function createCar(plate, brand, color) {
     return new Car(plate, color, brand);
 }
@@ -5,75 +6,96 @@ function submitCar() {
     var platebox = document.getElementById("plate").value;
     var brandbox = document.getElementById("brand").value;
     var colorbox = document.getElementById("color").value;
-    var carinfo = document.getElementById("carInfo");
-    var result = validCar(platebox, brandbox, colorbox);
-    if (result == true) {
-        createCar(platebox, brandbox, colorbox);
-    }
-    carinfo.innerText = "Plate number: " + platebox + " " + "Color: " + colorbox + " " + " Brand: " + brandbox;
-}
-function validCar(platebox, brandbox, colorbox) {
-    var plateValidation = (/^A-Za-z{3}0-9{4}/.test(platebox));
-    var brandValidation = (/^[a-z]+$/.test(brandbox));
-    var colorValidation = (/^[a-z]+$/.test(colorbox));
-    if (!plateValidation && !brandValidation && !colorValidation) {
-        alert("Your car information is not correct. Follow the given example.");
+    var plateValidation = (/^[0-9]{4}[a-zA-Z]{3}$/.test(platebox));
+    if (!plateValidation) {
+        alert("The plate is not correct, introduce 4 numbers and 3 letters.");
         return false;
     }
-    else if (platebox == "" || brandbox == "" || colorbox == "") {
-        alert("Please fill all the fields.");
+    else if (brandbox == "" || colorbox == "") {
+        alert("Please complete all the fields.");
+    }
+    else {
+        car = createCar(platebox, brandbox, colorbox);
+        showCar();
+        showWheelForm();
+    }
+}
+function showCar() {
+    var carinfo = document.getElementById("carInfo");
+    carinfo.innerText = "Plate number: " + car.plate + " " + "Color: " + car.color + " " + " Brand: " + car.brand;
+}
+function showWheelForm() {
+    document.getElementById("carForm").style.display = "none";
+    document.getElementById("wheelForm").style.display = "block";
+    document.getElementById("titleInfo").style.display = "block";
+}
+function createWheel(diameter, brand) {
+    car.addWheel(new Wheel(diameter, brand));
+}
+function submitWheels() {
+    //Marca de les rodes
+    var brandWheel1 = document.getElementById("brandWheel1").value;
+    var brandWheel2 = document.getElementById("brandWheel2").value;
+    var brandWheel3 = document.getElementById("brandWheel3").value;
+    var brandWheel4 = document.getElementById("brandWheel4").value;
+    //var result = testDiameter(diameter);
+    /*
+        if(result == true){
+            addWheel(diameter, brand);
+        }*/
+    /*
+    if(diam_wheel1 < 0.4 && diam_wheel1 > 2){
+        alert("Introduce correct diameter values.");
+        return false;
+    }else{
+        createWheel(diam_wheel1, brandWheel1);
+        createWheel(diam_wheel2, brandWheel2);
+        createWheel(diam_wheel3, brandWheel3);
+        createWheel(diam_wheel4, brandWheel4);
+    }*/
+    console.log(car);
+}
+function testDiameter(diameter) {
+    if (diameter < 0.4 && diameter > 2) {
         return false;
     }
     else {
         return true;
     }
 }
-// ***** DETECTA LA 1a CONDICIÓ DEL IF PERÒ DESPRÉS DÓNA TRUE I NO VALIDA
-function createWheel(diameter, brand) {
-    car.addWheel(new Wheel(diameter, brand));
-}
-// **** AIXÒ NO SÉ SI ESTÀ BEN ESTRUCTURAT, HE COPIAT EL FORMAT DEL "CREATECAR" - "car" surt com un error que no troba 
-function submitWheels() {
+function validateWheels() {
     // Diàmetre de les rodes
-    var wheel1 = document.getElementById("wheel1").value;
-    var wheel2 = document.getElementById("wheel2").value;
-    var wheel3 = document.getElementById("wheel3").value;
-    var wheel4 = document.getElementById("wheel4").value;
-    //Marca de les rodes
-    var brandWheel1 = document.getElementById("brandWheel1");
-    var brandWheel2 = document.getElementById("brandWheel2");
-    var brandWheel3 = document.getElementById("brandWheel3");
-    var brandWheel4 = document.getElementById("brandWheel4");
-    //Info des les rodes que es mostrarà per pantalla
+    var diam_wheel1 = Number(document.getElementById("wheel1").value);
+    var diam_wheel2 = Number(document.getElementById("wheel2").value);
+    var diam_wheel3 = Number(document.getElementById("wheel3").value);
+    var diam_wheel4 = Number(document.getElementById("wheel4").value);
+    var result = testDiameter();
+    if (diam)
+        ;
+}
+function showWheels() {
+    //Info des les rodes que es mostrarà per pantalla. WheelsInfo son els paragrafs html.
     var wheelsInfo1 = document.getElementById("wheelsInfo1");
     var wheelsInfo2 = document.getElementById("wheelsInfo2");
     var wheelsInfo3 = document.getElementById("wheelsInfo3");
     var wheelsInfo4 = document.getElementById("wheelsInfo4");
-    wheelsInfo1.innerText = "1st wheel: " + wheel1 + ", " + brandWheel1 + ". ";
-    wheelsInfo2.innerText = "2nd wheel: " + wheel2 + ", " + brandWheel2 + ". ";
-    wheelsInfo3.innerText = "3rd wheel: " + wheel3 + ", " + brandWheel3 + ". ";
-    wheelsInfo4.innerText = "4th wheel: " + wheel4 + ", " + brandWheel4 + ". ";
-    var resultWheels = validWheel(); //S'ha de posar algun parametre?
-    if (resultWheels == true) {
-        createWheel(diameter, brand);
-    }
+    wheelsInfo1.innerText = "1st wheel: " + diam_wheel1 + ", " + brandWheel1 + ". ";
+    wheelsInfo2.innerText = "2nd wheel: " + diam_wheel2 + ", " + brandWheel2 + ". ";
+    wheelsInfo3.innerText = "3rd wheel: " + diam_wheel3 + ", " + brandWheel3 + ". ";
+    wheelsInfo4.innerText = "4th wheel: " + diam_wheel4 + ", " + brandWheel4 + ". ";
     //m'ho detecta com un objecte.
 }
-// **** MASSES VARIABLES??
-function validWheel() {
+/*
+
+function validWheel(diameter: number){
     alert("validació rodes");
-    // var diameterValidation1 = (/^A-Za-z{3}0-9{4}/.test(wheel1));
-    // var diameterValidation2 = (/^A-Za-z{3}0-9{4}/.test(wheel2));
-    // var diameterValidation3 = (/^A-Za-z{3}0-9{4}/.test(wheel3));
-    // var diameterValidation4 = (/^A-Za-z{3}0-9{4}/.test(wheel4));
-    //var brandWheelValidation = (/^[a-z]+$/.test(brandbox));
-    var allWheels = [wheel1, wheel2, wheel3, wheel4];
-    if (!allWheels > 0.4 && !allWheels < 2) {
-        alert("Introduce the correct size.");
+    if(diameter < 0.4 && diameter > 2){
+        alert("Introduce correct diameter size.");
         return false;
-    }
-    else {
+    }else{
         return true;
     }
 }
+*/
 //VALIDACIÓ EM DÓNA ERROR
+//fer bé la validació i ocultar els formularis quan s'està executant un altre. vinculacio!!!
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,97 +1,107 @@
+let car: Car;
 
 function createCar(plate:string, brand:string, color:string){
     return new Car(plate, color, brand);
 }
 
-function submitCar(){
+//En prémer el botó "Create car" crea el cotxe, mostra els resultats i amaga carForm.   
+function submitCar(){ 
     const platebox = (<HTMLInputElement>document.getElementById("plate")).value;
     const brandbox = (<HTMLInputElement>document.getElementById("brand")).value;
     const colorbox = (<HTMLInputElement>document.getElementById("color")).value;
-    const carinfo = (<HTMLInputElement>document.getElementById("carInfo"));
 
-    var result = validCar(platebox,brandbox,colorbox);
-
-    if(result == true){
-        createCar(platebox,brandbox,colorbox);
-    }
-
-    carinfo.innerText = "Plate number: " + platebox + " " + "Color: " + colorbox + " " + " Brand: " + brandbox;
-}
-
-function validCar(platebox,brandbox,colorbox){
-    var plateValidation = (/^A-Za-z{3}0-9{4}/.test(platebox));
-    var brandValidation = (/^[a-z]+$/.test(brandbox));
-    var colorValidation = (/^[a-z]+$/.test(colorbox));
-
-    if(!plateValidation && !brandValidation && !colorValidation){
-        alert("Your car information is not correct. Follow the given example.");
+    var plateValidation = (/^[0-9]{4}[a-zA-Z]{3}$/.test(platebox)); 
+    if(!plateValidation){
+        alert("The plate is not correct, introduce 4 numbers and 3 letters.");
         return false;
-    }else if(platebox == "" || brandbox == "" || colorbox == ""){
-        alert("Please fill all the fields.");
-        return false;
+    }else if(brandbox == "" || colorbox == ""){
+        alert("Please complete all the fields.")
     }else{
-        return true;
+        car = createCar(platebox, brandbox, colorbox);
+        showCar();
+        showWheelForm();
     }
 }
-// ***** DETECTA LA 1a CONDICIÓ DEL IF PERÒ DESPRÉS DÓNA TRUE I NO VALIDA
 
-function createWheel(diameter: number, brand: string){
-    car.addWheel(new Wheel(diameter,brand));
+function showCar(){ 
+    const carinfo = (<HTMLInputElement>document.getElementById("carInfo"));
+    carinfo.innerText = "Plate number: " + car.plate + " " + "Color: " + car.color + " " + " Brand: " + car.brand;
 }
-// **** AIXÒ NO SÉ SI ESTÀ BEN ESTRUCTURAT, HE COPIAT EL FORMAT DEL "CREATECAR" - "car" surt com un error que no troba 
 
-function submitWheels(){
+function showWheelForm(){
+    document.getElementById("carForm").style.display = "none";
+    document.getElementById("wheelForm").style.display = "block";
+    document.getElementById("titleInfo").style.display = "block";
+}
+
+//Condició general per validar el diàmetre de les rodes.
+function testDiameter(diameter: number){
+    if(diameter > 0.4 && diameter < 2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function submitWheels(){    
+    // Marca de les rodes
+    var brandWheel1 = (<HTMLInputElement>document.getElementById("brandWheel1")).value;
+    var brandWheel2 = (<HTMLInputElement>document.getElementById("brandWheel2")).value;
+    var brandWheel3 = (<HTMLInputElement>document.getElementById("brandWheel3")).value;
+    var brandWheel4 = (<HTMLInputElement>document.getElementById("brandWheel4")).value;
+
     // Diàmetre de les rodes
-    const wheel1 = (<HTMLInputElement>document.getElementById("wheel1")).value;
-    const wheel2 = (<HTMLInputElement>document.getElementById("wheel2")).value;
-    const wheel3 = (<HTMLInputElement>document.getElementById("wheel3")).value;
-    const wheel4 = (<HTMLInputElement>document.getElementById("wheel4")).value;
+    var diam_wheel1 = Number((<HTMLInputElement>document.getElementById("wheel1")).value);
+    var diam_wheel2 = Number((<HTMLInputElement>document.getElementById("wheel2")).value);
+    var diam_wheel3 = Number((<HTMLInputElement>document.getElementById("wheel3")).value);
+    var diam_wheel4 = Number((<HTMLInputElement>document.getElementById("wheel4")).value);
 
-    //Marca de les rodes
-    var brandWheel1 = (<HTMLInputElement>document.getElementById("brandWheel1"));
-    var brandWheel2 = (<HTMLInputElement>document.getElementById("brandWheel2"));
-    var brandWheel3 = (<HTMLInputElement>document.getElementById("brandWheel3"));
-    var brandWheel4 = (<HTMLInputElement>document.getElementById("brandWheel4"));
+        if(!testDiameter(Number(diam_wheel1))){
+            alert("The diameter of the 1st wheel is not correct!");
+        }
+        if(!testDiameter(Number(diam_wheel2))){
+            alert("The diameter of the 2nd wheel is not correct!");
+        }
+        if(!testDiameter(Number(diam_wheel3))){
+            alert("The diameter of the 3rd wheel is not correct!");
+        }
+        if(!testDiameter(Number(diam_wheel4))){
+            alert("The diameter of the 4th wheel is not correct!");
+        }
 
-    //Info des les rodes que es mostrarà per pantalla
+    var wheel1 = new Wheel(Number(diam_wheel1), (brandWheel1));
+    var wheel2 = new Wheel(Number(diam_wheel2), (brandWheel2));
+    var wheel3 = new Wheel(Number(diam_wheel3), (brandWheel3));
+    var wheel4 = new Wheel(Number(diam_wheel4), (brandWheel4));
+
+    car.addWheel(wheel1);
+    car.addWheel(wheel2);
+    car.addWheel(wheel3);
+    car.addWheel(wheel4);
+
+    showWheels();
+    hideWheelsForm();
+}
+
+function showWheels(){
+    //Info des les rodes que es mostrarà per pantalla. WheelsInfo son els paragrafs html.
     const wheelsInfo1 = (<HTMLInputElement>document.getElementById("wheelsInfo1"));
     const wheelsInfo2 = (<HTMLInputElement>document.getElementById("wheelsInfo2"));
     const wheelsInfo3 = (<HTMLInputElement>document.getElementById("wheelsInfo3"));
     const wheelsInfo4 = (<HTMLInputElement>document.getElementById("wheelsInfo4"));
 
-    wheelsInfo1.innerText = "1st wheel: " + wheel1 + ", " + brandWheel1 + ". ";
-    wheelsInfo2.innerText = "2nd wheel: " + wheel2 + ", " + brandWheel2 + ". ";
-    wheelsInfo3.innerText = "3rd wheel: " + wheel3 + ", " + brandWheel3 + ". ";
-    wheelsInfo4.innerText = "4th wheel: " + wheel4 + ", " + brandWheel4 + ". ";
-
-
-    var resultWheels = validWheel();//S'ha de posar algun parametre?
-
-    if(resultWheels == true){
-        createWheel(diameter, brand);
-    }
-    //m'ho detecta com un objecte.
-}
-// **** MASSES VARIABLES??
-
-
-function validWheel(){
-    alert("validació rodes");
-    // var diameterValidation1 = (/^A-Za-z{3}0-9{4}/.test(wheel1));
-    // var diameterValidation2 = (/^A-Za-z{3}0-9{4}/.test(wheel2));
-    // var diameterValidation3 = (/^A-Za-z{3}0-9{4}/.test(wheel3));
-    // var diameterValidation4 = (/^A-Za-z{3}0-9{4}/.test(wheel4));
-    
-    //var brandWheelValidation = (/^[a-z]+$/.test(brandbox));
-    
-    var allWheels = [wheel1, wheel2, wheel3, wheel4];
-    if(!allWheels > 0.4 && !allWheels < 2){
-        alert("Introduce the correct size.");
-        return false;
-    }else{
-        return true;
-    }
-    
+    wheelsInfo1.innerText = "1st wheel: " + car.wheels[0].diameter + ", " + car.wheels[0].brand + ". ";
+    wheelsInfo2.innerText = "2nd wheel: " + car.wheels[1].diameter + ", " + car.wheels[1].brand + ". ";
+    wheelsInfo3.innerText = "3rd wheel: " + car.wheels[2].diameter + ", " + car.wheels[2].brand + ". ";
+    wheelsInfo4.innerText = "4th wheel: " + car.wheels[3].diameter + ", " + car.wheels[3].brand + ". ";
 }
 
-//VALIDACIÓ EM DÓNA ERROR
+function hideWheelsForm(){
+    document.getElementById("wheelForm").style.display = "none";
+}
+
+/*
+function createWheel(diameter: number, brand: string){
+    car.addWheel(new Wheel(diameter,brand));
+}
+*/
